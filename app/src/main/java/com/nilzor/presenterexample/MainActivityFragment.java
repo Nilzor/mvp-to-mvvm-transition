115d3946a2a6b4c372f34f21c6cadd3a41d7e503
Frode Nilsen <frode.nilsen@vg.no>
2015-07-17 13:06:09 +0200

    reimplemented with databinding

--- app/src/main/java/com/nilzor/presenterexample/MainActivityFragment.java
+++ app/src/main/java/com/nilzor/presenterexample/MainActivityFragment.java
@@ -1,103 +1,53 @@
 package com.nilzor.presenterexample;
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.CompoundButton;
-import android.widget.RadioButton;
-import android.widget.TextView;
 import android.widget.Toast;
-import com.hannesdorfmann.mosby.mvp.MvpFragment;
-import com.hannesdorfmann.mosby.mvp.MvpView;
-import butterknife.InjectView;
-import butterknife.OnClick;
 
-public class MainActivityFragment extends MvpFragment<MainActivityFragment, MainPresenter> implements MvpView {
-    @InjectView(R.id.username)
-    TextView mUsername;
+import com.nilzor.presenterexample.databinding.FragmentMainBinding;
 
-    @InjectView(R.id.password)
-    TextView mPassword;
-
-    @InjectView(R.id.newUserRb)
-    RadioButton mNewUserRb;
-
-    @InjectView(R.id.returningUserRb)
-    RadioButton mReturningUserRb;
-
-    @InjectView(R.id.loginOrCreateButton)
-    Button mLoginOrCreateButton;
-
-    @InjectView(R.id.email_block)
-    ViewGroup mEmailBlock;
-
-    @InjectView(R.id.loggedInUserCount)
-    TextView mLoggedInUserCount;
+public class MainActivityFragment extends Fragment {
+    private FragmentMainBinding mBinding;
+    private MainModel mViewModel;
 
     public MainActivityFragment() {
     }
 
     @Override
-    public MainPresenter createPresenter() {
-        return new MainPresenter();
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        mBinding = FragmentMainBinding.bind(view);
+        mViewModel = new MainModel(this, getResources());
+        mBinding.setData(mViewModel);
+        attachButtonListener();
+        return view;
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+    private void attachButtonListener() {
+        mBinding.loginOrCreateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mViewModel.logInClicked();
+            }
+        });
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        attachEventListeners();
+        ensureModelDataIsLodaded();
     }
 
-    private void attachEventListeners() {
-        mNewUserRb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                updateDependentViews();
-            }
-        });
-        mReturningUserRb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                updateDependentViews();
-            }
-        });
-    }
-
-    /** Prepares the initial state of the view upon startup */
-    public void setInitialState() {
-        mReturningUserRb.setChecked(true);
-        updateDependentViews();
-    }
-
-    /** Shows/hides email field and sets correct text of login button depending on state of radio buttons */
-    public void updateDependentViews() {
-        if (mReturningUserRb.isChecked()) {
-            mEmailBlock.setVisibility(View.GONE);
-            mLoginOrCreateButton.setText(R.string.log_in);
-        }
-        else {
-            mEmailBlock.setVisibility(View.VISIBLE);
-            mLoginOrCreateButton.setText(R.string.create_user);
+    private void ensureModelDataIsLodaded() {
+        if (!mViewModel.isLoaded()) {
+            mViewModel.loadAsync();
         }
     }
 
-    public void setNumberOfLoggedIn(int numberOfLoggedIn) {
-        mLoggedInUserCount.setText(""  + numberOfLoggedIn);
-    }
-
-    @OnClick(R.id.loginOrCreateButton)
-    public void loginOrCreate() {
-        if (mNewUserRb.isChecked()) {
-            Toast.makeText(getActivity(), "Please enter a valid email address", Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(getActivity(), "Invalid username or password", Toast.LENGTH_SHORT).show();
-        }
+    public void showShortToast(String text) {
+        Toast.makeText(getActivity(), text, Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/nilzor/presenterexample/MainModel.java
+++ app/src/main/java/com/nilzor/presenterexample/MainModel.java
@@ -1,31 +1,77 @@
 package com.nilzor.presenterexample;
 
+import android.content.res.Resources;
+import android.databinding.ObservableField;
 import android.os.AsyncTask;
-import android.os.Handler;
+import android.view.View;
+
 import java.util.Random;
 
 public class MainModel {
-    public Integer numberOfUsersLoggedIn;
+    public ObservableField<String> numberOfUsersLoggedIn = new ObservableField<>();
+    public ObservableField<Boolean> isExistingUserChecked = new ObservableField<>();
+    public ObservableField<Integer> emailBlockVisibility = new ObservableField<>();
+    public ObservableField<String> loginOrCreateButtonText = new ObservableField<>();
     private boolean mIsLoaded;
+    private MainActivityFragment mView;
+    private Resources mResources;
+
+    public MainModel(MainActivityFragment view, Resources resources) {
+        mView = view;
+        mResources = resources; // You might want to abstract this for testability
+        setInitialState();
+        updateDependentViews();
+        hookUpDependencies();
+    }
     public boolean isLoaded() {
         return mIsLoaded;
     }
 
-    public void loadAsync(final Handler.Callback onDoneCallback) {
+    private void setInitialState() {
+        numberOfUsersLoggedIn.set("...");
+        isExistingUserChecked.set(true);
+    }
+
+    private void hookUpDependencies() {
+        isExistingUserChecked.addOnPropertyChangedCallback(new android.databinding.Observable.OnPropertyChangedCallback() {
+            @Override
+            public void onPropertyChanged(android.databinding.Observable sender, int propertyId) {
+                updateDependentViews();
+            }
+        });
+    }
+
+    public void updateDependentViews() {
+        if (isExistingUserChecked.get()) {
+            emailBlockVisibility.set(View.GONE);
+            loginOrCreateButtonText.set(mResources.getString(R.string.log_in));
+        }
+        else {
+            emailBlockVisibility.set(View.VISIBLE);
+            loginOrCreateButtonText.set(mResources.getString(R.string.create_user));
+        }
+    }
+
+    public void loadAsync() {
         new AsyncTask<Void, Void, Void>() {
             @Override
             protected Void doInBackground(Void... params) {
                 // Simulating some asynchronous task fetching data from a remote server
                 try {Thread.sleep(2000);} catch (Exception ex) {};
-                numberOfUsersLoggedIn = new Random().nextInt(1000);
+                numberOfUsersLoggedIn.set("" + new Random().nextInt(1000));
                 mIsLoaded = true;
                 return null;
             }
-
-            @Override
-            protected void onPostExecute(Void aVoid) {
-                onDoneCallback.handleMessage(null);
-            }
         }.execute((Void) null);
+    }
+
+    public void logInClicked() {
+        // Illustrating the need for calling back to the view though testable interfaces.
+        if (isExistingUserChecked.get()) {
+            mView.showShortToast("Invalid username or password");
+        }
+        else {
+            mView.showShortToast("Please enter a valid email address");
+        }
     }
 }
