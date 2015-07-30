33676b62c3f5b80209a1268c4a0a0f3a4b8c6901
Frode Nilsen <frode.nilsen@vg.no>
2015-07-30 09:26:13 +0200

    Add a second activity to illustrate how the first one is Garbage Collected when finished. Even though there are circular dependencies, this does not pose a problem with regards to memory leaks in this example.

--- app/src/main/java/com/nilzor/presenterexample/SecondActivity.java
+++ app/src/main/java/com/nilzor/presenterexample/SecondActivity.java
@@ -1,22 +1,15 @@
 package com.nilzor.presenterexample;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void skipLogin(View view) {
-        startActivity(new Intent(this, SecondActivity.class));
-        finish();
+        setContentView(R.layout.activity_second);
     }
 }
