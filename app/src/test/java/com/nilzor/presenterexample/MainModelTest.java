a787b700114c3d09e1b3a526bac6640353fc5baa
Frode Nilsen <frode.nilsen@vg.no>
2015-11-13 13:31:39 +0100

    Write first unit test against MainModel, utilizing Mockito and mocked ToastPresenter

---
+++ app/src/test/java/com/nilzor/presenterexample/MainModelTest.java
@@ -0,0 +1,29 @@
+package com.nilzor.presenterexample;
+
+import android.content.res.Resources;
+
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
+public class MainModelTest {
+    @Captor
+    ArgumentCaptor<String> mStringCaptor = ArgumentCaptor.forClass(String.class);
+
+    // Method name format following Roy Osherove's structure of:
+    //    UnitOfWork_StateUnderTest_ExpectedBehavior
+
+    @Test
+    public void loginClicked_existingUserWrongCredentials_errorToastShown() {
+        ToastPresenter toast = mock(ToastPresenter.class);
+        Resources resources = mock(Resources.class);
+        MainModel model = new MainModel(toast, resources);
+        model.isExistingUserChecked.set(true);
+        model.logInClicked(null);
+        verify(toast).showShortToast(mStringCaptor.capture());
+        assertEquals("Invalid username or password", mStringCaptor.getValue());
+    }
+}
