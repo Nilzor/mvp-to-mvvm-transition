a2948e6494e9a15abffe9ce2398a7797d299cad4
Frode Nilsen <nilzor@gmail.com>
2015-11-21 15:13:05 +0100

    Remove non-working uiToModel attempt

--- app/src/main/java/com/nilzor/presenterexample/ui/LoginFragment.java
+++ app/src/main/java/com/nilzor/presenterexample/ui/LoginFragment.java
@@ -9,7 +9,6 @@ import android.view.ViewGroup;
 import com.nilzor.presenterexample.R;
 import com.nilzor.presenterexample.databinding.FragmentLoginBinding;
 import com.nilzor.presenterexample.viewmodels.LoginFragmentViewModel;
-import com.nilzor.presenterexample.wrappers.EditTextHelper;
 import com.nilzor.presenterexample.wrappers.ToastPresenter;
 
 public class LoginFragment extends Fragment {
@@ -26,16 +25,7 @@ public class LoginFragment extends Fragment {
         ToastPresenter toastPresenter = new ToastPresenter(getActivity().getApplicationContext());
         mViewModel = new LoginFragmentViewModel(toastPresenter, getResources());
         mBinding.setData(mViewModel);
-        EditTextHelper.bindOnChangeListener(mBinding.username, this::uiToModel);
-        EditTextHelper.bindOnChangeListener(mBinding.password, this::uiToModel);
         return view;
-    }
-
-    private void uiToModel() {
-        mBinding.unbind();
-        mViewModel.password.set(mBinding.password.getText().toString());
-        mViewModel.username.set(mBinding.username.getText().toString());
-        //mBinding.setData(mViewModel);
     }
 
     @Override
